67b45fbd029620bbe86b08c4c1f96a7082668754
correcherr <lopezoscarcorrecher@gmail.com>
2026-01-16 12:45:16 +0100

    Cambio de estructura de proyecto

---
+++ Proyecto2DAM/src/main/java/com/proyectodam/Proyecto2DAM/Proyecto2DamApplication.java
@@ -0,0 +1,13 @@
+package com.proyectodam.Proyecto2DAM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto2DamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto2DamApplication.class, args);
+	}
+
+}
---
+++ Proyecto2DAM/src/test/java/com/proyectodam/Proyecto2DAM/Proyecto2DamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectodam.Proyecto2DAM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto2DamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
